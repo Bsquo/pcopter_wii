c80bb1e767f24fb9f2e9305fa9c0ee7a2a81dae9
unknown <88517417+Bsquo@users.noreply.github.com>
2022-07-26 19:23:57 +0100

    More `CVFlag` decompilation

--- include/game/utils/VFlag.h
+++ include/game/utils/VFlag.h
@@ -15,16 +15,20 @@ class CVFlag {
         // Clear__6CVFlagFv (0x800267b8)
         void Clear();
         // Set__6CVFlagFUsi (0x800267c4)
-        void Set(u16, s32);
+        void Set(u16, int);
         // Check__6CVFlagFUs (0x800267f4)
-        s32 Check(u16);
-
+        u32 Check(u16);
         // __as__6CVFlagFRC6CVFlag (0x8002f994)
         void operator=(const CVFlag&);
-
         // vtable (__vt__6CVFlag): 0x801cb7a8
         // __dt__6CVFlagFv (0x80026778)
         virtual ~CVFlag();
+        // This vtable has an extra 4 bytes at the end that are all 0.
+        // Those extra 0 correspond to this pure virtual function that is declared
+        // after the virtual destructor declaration.
+        // https://www.tutorialspoint.com/pure-virtual-functions-and-abstract-classes-in-cplusplus#
+        // (Thanks Seeky from the GC/Wii Decompilation discord server for telling me about this!)
+        virtual void unknown_pure_virtual_function() = 0;
 };
 
 #endif // VFLAG_H
--- src/game/utils/VFlag.cpp
+++ src/game/utils/VFlag.cpp
@@ -1,20 +1,25 @@
 #include "include/game/utils/VFlag.h"
 
 CVFlag::CVFlag(): mFlag(0) {}
-/*
+
 CVFlag::~CVFlag() {}
 
 void CVFlag::Clear() {
     mFlag = 0;
 } 
 
-void CVFlag::Set(u16 flag, s32 enableFlag) {
-    s32 a = 1 << flag;
-    // andc r0,r0,r5 ---> mFlag = mFlag & ~enableFlag
-    (enableFlag != 0) ? (mFlag = mFlag | a) : (mFlag = mFlag & ~enableFlag);
+// Thanks to Chippy from the GC/Wii Decompilation discord server for helping matching this function!
+// https://decomp.me/scratch/8p2Ll
+void CVFlag::Set(u16 flag, int enableFlag) {
+    u32 temp = 1;
+    temp = (temp << flag);
+    (enableFlag != 0) ? (mFlag = mFlag | temp) : (mFlag = mFlag & ~temp);
 }
 
-s32 CVFlag::Check(u16 flag) {
-
+// Thanks to Chippy from the GC/Wii Decompilation discord server for helping matching this function!
+// https://decomp.me/scratch/gXCNG
+u32 CVFlag::Check(u16 flag) {
+    u32 temp = 1;
+    temp = (temp << flag);
+    return (mFlag & temp) != 0;
 }
-*/
